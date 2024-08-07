fb66b1402d936dc82945c6911d63e127afe067dc
TheSnewby <stephen.j.newby@gmail.com>
2024-08-07 12:21:57 -0500

    atlas-binary_trees, also checking whether children need to be set to NULL

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node
+ * @value: value in new node
+ *
+ * Return: pointer to new NODE, NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+
+	return new_node;
+}
