d71dcc116a14253429566613696f78f902ac02ee
TheSnewby <stephen.j.newby@gmail.com>
2024-08-09 17:39:07 -0500

    made pretty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,13 +13,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left && tree->right)
-		return (1 + binary_tree_size(tree->left) +
-		binary_tree_size(tree->right));
-	else if (tree->right)
-		return (1 + binary_tree_size(tree->right));
-	else if (tree->left)
-		return (1 + binary_tree_size(tree->left));
-	else
-		return (1);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
