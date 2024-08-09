c8039043a53a646edd58937cc7f13a62003b5223
TheSnewby <stephen.j.newby@gmail.com>
2024-08-09 11:54:39 -0500

    inorder traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - traverses a tree in order
+ * @tree: tree
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	if (tree->left)
+		binary_tree_inorder(tree->left, func);
+
+	if (tree->right)
+		binary_tree_inorder(tree->right, func);
+	func(tree->n);
+}
