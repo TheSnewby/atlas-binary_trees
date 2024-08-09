ae3c42bf8f6bb43c47a93ea0fdf96165c4d8fc84
TheSnewby <stephen.j.newby@gmail.com>
2024-08-09 14:42:13 -0500

    added helper functiond description and missing left-right calls

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,12 +18,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (bth_helper(tree, height));
 }
 
+/**
+ * bth_helper - recursive function to find height
+ * @tree: node of tree
+ * @height: height
+ *
+ * Return: height
+ */
 size_t bth_helper(const binary_tree_t *tree, size_t height)
 {
 	size_t height_L = 0, height_R = 0;
+
 	if (tree->left)
-		height_L = bth_helper(tree, 1 + height);
+		height_L = bth_helper(tree->left, 1 + height);
 	if (tree->right)
-		height_R = bth_helper(tree, 1 + height);
+		height_R = bth_helper(tree->right, 1 + height);
 	return (height_L > height_R ? height_L : height_R);
 }
