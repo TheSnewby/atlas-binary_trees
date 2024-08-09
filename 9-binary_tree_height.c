1e74d8b5af2639598cbd9ecd949be71286b37e2d
TheSnewby <stephen.j.newby@gmail.com>
2024-08-09 15:11:32 -0500

    backup before trying a different method

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (bth_helper(tree, height));
 }
-
+// probably don't need
 /**
  * bth_helper - recursive function to find height
  * @tree: node of tree
