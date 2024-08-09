43626ac4dcea1eacac3540ff9bdff4f3870de65a
TheSnewby <stephen.j.newby@gmail.com>
2024-08-09 11:57:41 -0500

    updated comments in inorder, copied incorrect soln to postorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -5,14 +5,14 @@
  * @tree: tree
  * @func: passed function
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree)
 		return;
 	if (tree->left)
-		binary_tree_inorder(tree->left, func);
+		binary_tree_postorder(tree->left, func);
 	if (tree->right)
-		binary_tree_inorder(tree->right, func);
+		binary_tree_postorder(tree->right, func);
 	if (func)
 		func(tree->n);
 }
