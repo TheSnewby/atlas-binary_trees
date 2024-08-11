975ec964c9ea5092705be5f0957931c31ee6f56c
TheSnewby <stephen.j.newby@gmail.com>
2024-08-11 14:22:23 -0500

    uncle finder

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: node to look for uncle
+ *
+ * Return: uncle or NULL otherwise
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (node->parent)
+		if (node->parent->parent)
+			return (btu_helper(node->parent->parent, node->parent));
+	return (NULL);
+}
+
+/**
+ * btu_helper - uncle finder
+ * @grandparent: grandparent of original node
+ * @parent: parent of original node
+ *
+ * Return: uncle or NULL
+ */
+binary_tree_t *btu_helper(binary_tree_t *grandparent, binary_tree_t *parent)
+{
+	if (grandparent->left && grandparent->left != parent)
+		return (grandparent->left);
+	else if (grandparent->right && grandparent->right != parent)
+		return (grandparent->right);
+	return (NULL);
+}
