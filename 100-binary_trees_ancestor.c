7c85a8bc1ff9d399aa985987c8300430bf959799
TheSnewby <stephen.j.newby@gmail.com>
2025-07-02 16:17:35 -0500

    103-binary_tree_rotate_left

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * bta_helper - recursively searches for the LCA
+ * @
+ */
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: first node
+ * @second: second node
+ *
+ * Return: pointer to the lowest common answer, or NULL if none found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second)
+{
+	if (!first || !second || first == second)
+		return (NULL);
+
+	
+}
---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - rotates a binary tree to the left
+ * @tree: the passed tree or subtree
+ *
+ * Return: new root
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *temp;
+
+	if (!tree)
+		return (NULL);
+
+	tree->right->parent = tree->parent; /* assign old root's parent to new root's parent */
+	tree->parent = tree->right; /* assign old root's parent to new root */
+
+	if (tree->right->left)
+	{
+		tree->right->left->parent = tree; /* assign new root's L-child's parent to old root */
+		tree->right = tree->right->left; /* assign old root's R-child to new root's old L-child */
+	}
+
+	tree->right->left = tree; /* change new root's left to old root */
+
+	return tree->parent;
+}
