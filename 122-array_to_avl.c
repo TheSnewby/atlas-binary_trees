83a03c664eb25de1ed8fe90deaf340ede90e428a
TheSnewby <stephen.j.newby@gmail.com>
2025-07-03 09:56:54 -0500

    added a dereference

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -17,7 +17,7 @@ avl_t *array_to_avl(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
-		if (!avl_insert(&tree, array[i]))
+		if (!avl_insert(&(*tree), array[i]))
 			return (NULL);
 	}
 
