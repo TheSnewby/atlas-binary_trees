318bdf5f191e355ad919e6b1f52b4c80928e2f40
TheSnewby <stephen.j.newby@gmail.com>
2025-07-03 10:05:35 -0500

    initialized the new tree to NULL

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -10,14 +10,14 @@
 avl_t *array_to_avl(int *array, size_t size)
 {
 	size_t i;
-	avl_t *tree;
+	avl_t *tree = NULL;
 
 	if (!array || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		if (!avl_insert(&tree, array[i]))
+		if (!avl_insert(&tree, array[i]) && !tree)
 			return (NULL);
 	}
 
