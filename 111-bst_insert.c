8e6f5f5d6609377b0ec00f2b784e10b929a25c2e
TheSnewby <stephen.j.newby@gmail.com>
2025-07-10 20:55:43 -0500

    fully removed balancer code

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,54 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert_balancer - balances during the bst_insert()
- * @tree: double pointer to a node
- * @value: value being added
- */
-void bst_insert_balancer(bst_t **tree, int value)
-{
-	int	balance = binary_tree_balance(*tree);
-
-	/* LL Case */
-	if (balance > 1 && value < (*tree)->left->n)
-	{
-		*tree = binary_tree_rotate_right(*tree);
-		if (*tree && (*tree)->parent == NULL)
-			(*tree)->parent = NULL;
-	}
-
-	/* RR Case */
-	else if (balance < -1 && value > (*tree)->right->n)
-	{
-		*tree = binary_tree_rotate_left(*tree);
-		if (*tree && (*tree)->parent == NULL)
-			(*tree)->parent = NULL;
-	}
-
-	/* LR Case */
-	else if (balance > 1 && value > (*tree)->left->n)
-	{
-		(*tree)->left = binary_tree_rotate_left((*tree)->left);
-		if ((*tree)->left)
-			(*tree)->left->parent = *tree;
-		*tree = binary_tree_rotate_right(*tree);
-		if (*tree && (*tree)->parent == NULL)
-			(*tree)->parent = NULL;
-	}
-
-	/* RL Case */
-	else if (balance < -1 && value < (*tree)->right->n)
-	{
-		(*tree)->right = binary_tree_rotate_right((*tree)->right);
-		if ((*tree)->right)
-			(*tree)->right->parent = *tree;
-		*tree = binary_tree_rotate_left(*tree);
-		if (*tree && (*tree)->parent == NULL)
-			(*tree)->parent = NULL;
-	}
-}
-
-/**
  * bst_insert - insert a node into a BST
  * @tree: double pointer node
  * @value: value to be inserted in a node
@@ -82,8 +34,6 @@ bst_t *bst_insert(bst_t **tree, int value)
 
 	if (new_node == NULL)
 		return (NULL);
-
-	/* bst_insert_balancer(tree, value); */
 
 	return (new_node);
 }
